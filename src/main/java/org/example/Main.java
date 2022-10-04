7d0cf9b36fc1c42d6210ddb36062ff6d3675c4c0
chihirooooooooo <107065792+chihirooooooooo@users.noreply.github.com>
2022-10-04 00:05:30 -0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        private static Connection getConnection() throws URISyntaxException, SQLException {
+           String dbUrl = System.getenv("postgres://kexafudwppoppl:c0abaa9ed698fdce77c4c79079ca966d7b06eb9f7a524cb3db5a90faf9c8eb6c@ec2-54-147-36-107.compute-1.amazonaws.com:5432/deum74j36kqraj");
+           return DriverManager.getConnection(dbUrl);
+        }
     }
 }
